7bb4a308ebd94d6bdfe69efa5d06773278f60406
Simon Detheridge <simon@widgit.com>
2014-09-02 15:35:18 +0100

    Improve metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,18 @@
 name             'apt-upgrade-once'
 maintainer       'Simon Detheridge'
 maintainer_email 'simon@widgit.com'
-license          'apachev2'
+license          'Apache v2.0'
 description      'Runs apt-get upgrade once'
 long_description 'Upgrades all packages on a debian or ubuntu box, but only once'
 version          '0.1.0'
+attribute        'apt-upgrade-once/statusfile',
+  :display_name => 'Status file location',
+  :description => 'Location of status file that prevents packages being re-upgraded',
+  :type => 'string',
+  :required => 'optional',
+  :default => '/etc/.apt-upgrade-run'
 
+recipe "apt-upgrade-once::default", "Upgrades system packages on first run"
+
+supports 'ubuntu'
+supports 'debian'
